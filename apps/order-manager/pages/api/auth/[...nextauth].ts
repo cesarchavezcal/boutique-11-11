c16ff1c738515b1978168b5af12e115eb8f56b57
César Chávez <cesarchavezcal@gmail.com>
2022-07-14 15:04:32 -0500

    Added IG

--- apps/order-manager/pages/api/auth/[...nextauth].ts
+++ apps/order-manager/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import FacebookProvider from 'next-auth/providers/facebook';
+import InstagramProvider from 'next-auth/providers/instagram';
 import GoogleProvider from 'next-auth/providers/google';
 import NextAuth, { NextAuthOptions } from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
@@ -16,6 +17,11 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
+    InstagramProvider({
+      clientId: process.env.INSTAGRAM_CLIENT_ID,
+      clientSecret: process.env.INSTAGRAM_CLIENT_SECRET,
+    }),
+
     // GoogleProvider({
     //   clientId: process.env.GOOGLE_ID,
     //   clientSecret: process.env.GOOGLE_SECRET,
